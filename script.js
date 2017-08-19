5c0073dec086d9e8630feb2ef05732e863eaa9fa
Smith1337 <smithsmite2@gmail.com>
2017-08-19 09:03:11 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //initialize Google Maps
 function initMap() {
-  var location = {lat:  43.64393, lng: -79.378189};
+  var location = {lat:  43.641655, lng: -79.389332};
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 15,
     center: location
