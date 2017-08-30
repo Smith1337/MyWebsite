9a120084f7d7195338b8968f24c0aba9960aa5fa
Smith1337 <smithsmite2@gmail.com>
2017-08-30 12:36:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 //initialize Google Maps
 function initMap() {
   var location = {lat:  43.64393, lng: -79.378189};
-  var map = new google.maps.Map(document.getElementById('map'), {
+  var map = new google.maps.Map(document.getElementById('map1'), {
+    zoom: 15,
+    center: location
+  });
+  var marker = new google.maps.Marker({
+    position: location,
+    map: map
+  });
+}
+function initMap() {
+  var location = {lat:  43.727182, lng: -79.450852};
+  var map = new google.maps.Map(document.getElementById('map2'), {
     zoom: 15,
     center: location
   });
